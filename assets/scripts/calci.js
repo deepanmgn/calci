b45a89e542c1e6e72df13f87551531a84179b1f3
Deepan Muruan <deep.mgn@gmail.com>
2015-09-07 12:11:07 +0530

    User can now enter digits by pressing digits on keyboard

--- assets/scripts/calci.js
+++ assets/scripts/calci.js
@@ -19,4 +19,10 @@ $(document).ready(function() {
 		$('#preview').html('');
 		$('#result').html('');
 	});
+
+	['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'].forEach(function(digit) {
+		$(document).bind('keyup', digit, function() {
+			$('#preview').html($('#preview').html() + digit);
+	});
+});
 });
